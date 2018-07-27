112ce3ea3f694633e0f4e065971b72445f2a39d5
Kyla Ulibarri <k.barri.071@gmail.com>
2018-07-27 16:24:23 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,8 +8,18 @@ class Dog
   def name
     @name
   end
+  
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  
+  def breed
+    @breed
+  end
 end
 
 spot = Dog.new
 spot.name = "Spot"
+spot.breed = "Dachshund"
 puts spot.name
+puts spot.breed
